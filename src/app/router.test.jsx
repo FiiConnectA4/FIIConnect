aac9ac645b942efd98f91235168046a0ba63485c
Roberto <robertospiridon1@gmail.com>
2025-04-10 09:26:18 +0300

    feat: update frontend orar

--- src/app/router.test.jsx
+++
@@ -1,8 +0,0 @@
-const { render, screen } = require('@testing-library/react');
-const Router = require('./Router'); // Adjust the import based on your actual Router component path
-
-test('renders router component', () => {
-	render(<Router />);
-	const linkElement = screen.getByText(/router/i); // Adjust the text based on your Router component
-	expect(linkElement).toBeInTheDocument();
-});
