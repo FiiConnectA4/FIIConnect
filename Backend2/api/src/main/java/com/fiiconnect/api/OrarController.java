73ebafa55864fbf78a09bbd34d10a7a65268c523
rares4racu <161528740+rares4racu@users.noreply.github.com>
2025-04-14 15:53:06 +0300

    Add files via upload

---
+++ Backend2/api/src/main/java/com/fiiconnect/api/OrarController.java
@@ -0,0 +1,46 @@
+package com.fiiconnect.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orar")
+public class OrarController {
+
+    @Autowired
+    private OrarRepository repository;
+
+    @GetMapping
+    public List<com.fiiconnect.api.Orar> toateOrele() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public com.fiiconnect.api.Orar getOrarById(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @PostMapping
+    public com.fiiconnect.api.Orar adaugaOrar(@RequestBody com.fiiconnect.api.Orar orar) {
+        return repository.save(orar);
+    }
+
+    @DeleteMapping("/{id}")
+    public void stergeOrar(@PathVariable Integer id) {
+        repository.deleteById(id);
+    }
+
+    // Endpoint pentru a obține orarul pe baza grupei
+    @GetMapping("/grupa/{an}/{grupa}")
+    public List<Orar> getOrarByAnAndGrupa(@PathVariable String an, @PathVariable String grupa) {
+    return repository.findByAnAndGrupa(an, grupa);
+}
+
+    // Endpoint pentru a obține orarul pe baza numelui profesorului
+    @GetMapping("/profesor/{profesor}")
+    public List<Orar> getOrarByProfesor(@PathVariable String profesor) {
+        return repository.findByProfesor(profesor);
+    }
+}
