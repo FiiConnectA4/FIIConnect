7a45836996bd0c464a96785b4d02b3db0e41fcf6
rares4racu <161528740+rares4racu@users.noreply.github.com>
2025-04-14 15:53:06 +0300

    Add files via upload
    fix rebase

--- Backend2/api/src/main/java/com/fiiconnect/api/OrarRepository.java
+++ Backend2/api/src/main/java/com/fiiconnect/api/OrarRepository.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.fiiconnect.api;
 import java.util.List;
 
@@ -10,3 +11,17 @@ public interface OrarRepository extends JpaRepository<com.fiiconnect.api.Orar, I
     List<Orar> findByProfesor(String profesor);
 }
 
+=======
+package com.fiiconnect.api;
+import java.util.List;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrarRepository extends JpaRepository<com.fiiconnect.api.Orar, Integer> {
+    List<Orar> findByAnAndGrupa(String an, String grupa);
+    List<Orar> findByProfesor(String profesor);
+}
+
+>>>>>>> 73ebafa5 (Add files via upload)
--- Backend2/api/src/main/java/com/fiiconnect/api/SalaRepository.java
+++ Backend2/api/src/main/java/com/fiiconnect/api/SalaRepository.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.fiiconnect.api;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -5,3 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface SalaRepository extends JpaRepository<Sala, Long> {
     
 }
+=======
+package com.fiiconnect.api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalaRepository extends JpaRepository<Sala, Long> {
+    
+}
+>>>>>>> 73ebafa5 (Add files via upload)
