05d61abf400901fe8b2212a0741a2524cd0f55f5
rares4racu <161528740+rares4racu@users.noreply.github.com>
2025-04-28 21:18:37 +0300

    Add files via upload

---
+++ FIIConnect-demo2/Backend2/api/src/main/java/com/fiiconnect/api/Sala.java
@@ -0,0 +1,59 @@
+package com.fiiconnect.api;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "sali")
+public class Sala {
+    @Id
+    @SequenceGenerator(name = "sala_seq", sequenceName = "SALA_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sala_seq")
+    private Long id;
+
+    private String nume;
+    private Integer capacitate;
+    private String tipSala;
+    private String locatie;
+    private String imagineUrl;
+    private String dotari;
+    private String observatii;
+
+    public Sala() {}
+
+    public Sala(String nume, Integer capacitate, String tipSala, String locatie,
+                String imagineUrl, String dotari, String observatii) {
+        this.nume = nume;
+        this.capacitate = capacitate;
+        this.tipSala = tipSala;
+        this.locatie = locatie;
+        this.imagineUrl = imagineUrl;
+        this.dotari = dotari;
+        this.observatii = observatii;
+    }
+
+    // Getters și Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNume() { return nume; }
+    public void setNume(String nume) { this.nume = nume; }
+
+    public Integer getCapacitate() { return capacitate; }
+    public void setCapacitate(Integer capacitate) { this.capacitate = capacitate; }
+
+    public String getTipSala() { return tipSala; }
+    public void setTipSala(String tipSala) { this.tipSala = tipSala; }
+
+    public String getLocatie() { return locatie; }
+    public void setLocatie(String locatie) { this.locatie = locatie; }
+
+    public String getImagineUrl() { return imagineUrl; }
+    public void setImagineUrl(String imagineUrl) { this.imagineUrl = imagineUrl; }
+
+    public String getDotari() { return dotari; }
+    public void setDotari(String dotari) { this.dotari = dotari; }
+
+    public String getObservatii() { return observatii; }
+    public void setObservatii(String observatii) { this.observatii = observatii;
+     }
+    }
---
+++ FIIConnect-demo2/Backend2/api/src/main/java/com/fiiconnect/api/SalaController.java
@@ -0,0 +1,92 @@
+package com.fiiconnect.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/sali")
+public class SalaController {
+
+    @Autowired
+    private SalaRepository salaRepository;
+
+    @GetMapping
+    public List<Sala> getAll() {
+        return salaRepository.findAll();
+    }
+
+    @PostMapping
+    public Sala create(@RequestBody Sala sala) {
+        return salaRepository.save(sala);
+    }
+
+    @GetMapping("/id/{id}")
+    public Sala getById(@PathVariable Long id) {
+        return salaRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void stergeSala(@PathVariable Long id) {
+        salaRepository.deleteById(id); // șterge înregistrările din DB după id
+    }
+
+    @GetMapping("/nume/{nume}")
+    public List<SalaDTO> getSalabyNume(@PathVariable String nume) { 
+        // Obținem lista de orare din baza de date
+        List<Sala> SalaList = salaRepository.findByNume(nume);
+
+        // Mapează entitățile Orar în OrarDTO, fără a include frecventa
+        return SalaList.stream().map(sala -> {
+            // Creăm obiectul DTO
+            return new SalaDTO(
+                sala.getCapacitate(),
+                sala.getDotari(),
+                sala.getObservatii(),
+                sala.getLocatie()
+            );
+        }).collect(Collectors.toList());
+    }
+
+@PutMapping("/{id}")
+public ResponseEntity<com.fiiconnect.api.Sala> updateSala(@PathVariable Long id, @RequestBody com.fiiconnect.api.Sala sala){
+    com.fiiconnect.api.Sala existingSala = salaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Sala not found with id " + id));
+    
+    if (sala.getNume() != null) {
+        existingSala.setNume(sala.getNume());
+    }
+
+    if (sala.getCapacitate() != null) {
+        existingSala.setCapacitate(sala.getCapacitate());
+    }
+
+    if (sala.getTipSala() != null) {
+        existingSala.setTipSala(sala.getTipSala());
+    }
+
+    if (sala.getLocatie() != null) {
+        existingSala.setLocatie(sala.getLocatie());
+    }
+    
+    if (sala.getImagineUrl() != null) {
+        existingSala.setImagineUrl(sala.getImagineUrl());
+    }
+
+    if (sala.getDotari() != null) {
+        existingSala.setDotari(sala.getDotari());
+    }
+
+    if (sala.getObservatii() != null) {
+        existingSala.setObservatii(sala.getObservatii());
+    }
+
+    com.fiiconnect.api.Sala updatedSala = salaRepository.save(existingSala);
+    return ResponseEntity.ok(updatedSala);
+
+}
+
+}
