fd67911cbcc8fa32c1f98885389b9b952166fdd3
TudorIsCoding <tudor2016tudor@outlook.com>
2025-05-03 12:51:35 +0300

    added jacoco plugin for code coverage and changed build.yml in order for the sonarqube to see the coverage

--- backend/api/src/main/java/com/fiiconnect/api/didactic/configs/SftpServerInfo.java
+++ backend/api/src/main/java/com/fiiconnect/api/didactic/configs/SftpServerInfo.java
@@ -11,8 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "sftp")
 public class SftpServerInfo {
-    @Getter
-    @Setter
     private String host;
     private Integer port;
     private String username;
