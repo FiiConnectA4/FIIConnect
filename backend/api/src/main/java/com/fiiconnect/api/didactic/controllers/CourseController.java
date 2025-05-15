404347a640d5ac7b0bfe0219fe80e055c1f047ae
unknown-characteristics <78977619+unknown-characteristics@users.noreply.github.com>
2025-05-15 14:39:30 +0300

    didactic: delete unused import

--- backend/api/src/main/java/com/fiiconnect/api/didactic/controllers/CourseController.java
+++ backend/api/src/main/java/com/fiiconnect/api/didactic/controllers/CourseController.java
@@ -21,7 +21,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.management.DescriptorKey;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
