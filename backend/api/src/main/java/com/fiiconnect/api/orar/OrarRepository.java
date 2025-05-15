8d5f1777b7b3de93105ac2db1d25a3bb593dcaa8
RobertoSpiridon <152715649+RobertoSpy@users.noreply.github.com>
2025-05-15 17:59:59 +0300

    Delete backend/api/src/main/java/com/fiiconnect/api/orar directory

--- backend/api/src/main/java/com/fiiconnect/api/orar/OrarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fiiconnect.api.orar;
-
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrarRepository extends JpaRepository<com.fiiconnect.api.orar.Orar, Integer> {
-}
