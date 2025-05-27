b7ade100f0adc97de28e4bdead95be42362f51f3
DvkaN <vlad.cires@gmail.com>
2025-05-27 22:30:31 +0300

    Fixed: Sidebar not being fixed, now fixed to left side.

--- frontend/src/layouts/StudentLayout.jsx
+++ frontend/src/layouts/StudentLayout.jsx
@@ -1,30 +1,35 @@
+// src/layouts/StudentLayout.jsx
+import React from "react";
+import { Outlet } from "react-router-dom";
+
 import StudentSidebar from "../components/Sidebar-Student/StudentSidebar";
 import NotificationBell from "../components/notifications/NotificationBell";
 import LogoutButton from "../components/logout-button/LogoutButton";
-import { Outlet } from "react-router-dom";
 
-const StudentLayout = () => {
-    return (
-        <div style={{ display: "flex", height: "100vh" }}>
-            <StudentSidebar />
-            <div style={{ flex: 1, padding: "2rem" }}>
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "flex-end",
-                        alignItems: "center",
-                        gap: "1rem",
-                        marginBottom: "1rem",
-                    }}
-                >
-                    <NotificationBell />
-                    <LogoutButton />
-                </div>
+import "../styles/StudentLayout.css";
 
-                <Outlet />
-            </div>
+const StudentLayout = () => (
+    <div className="layout">
+        {/* 1) Sidebar-ul rămâne FIXED */}
+        <StudentSidebar />
+
+        {/* 2) Conținutul începe după 240px */}
+        <div className="content">
+            <header
+                style={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    alignItems: "center",
+                    gap: "1rem",
+                    marginBottom: "1rem",
+                }}
+            >
+                <NotificationBell />
+                <LogoutButton />
+            </header>
+            <Outlet />
         </div>
-    );
-};
+    </div>
+);
 
 export default StudentLayout;
