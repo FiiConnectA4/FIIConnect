987e1ef858fea61164e8daa46ce89ec84b2a0b8c
Stefan Manjescu <stefanmanjescu@gmail.com>
2025-05-27 17:08:44 +0300

    am adaugat clasa pt managementul tagurilor

--- backend/api/src/main/java/com/fiiconnect/api/social_secretary/DTO/TagDTO.java
+++ backend/api/src/main/java/com/fiiconnect/api/social_secretary/DTO/TagDTO.java
@@ -3,10 +3,19 @@ package com.fiiconnect.api.social_secretary.DTO;
 import com.fiiconnect.api.social_secretary.enums.TagType;
 
 public class TagDTO {
+    private Long id;
     private String name;
     private TagType type;
 
+    public TagDTO() {}
+
     public TagDTO(String name, TagType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public TagDTO(Long id, String name, TagType type) {
+        this.id = id;
         this.name = name;
         this.type = type;
     }
@@ -27,10 +36,19 @@ public class TagDTO {
         this.type = type;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "TagDTO{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", type=" + type +
                 '}';
     }
