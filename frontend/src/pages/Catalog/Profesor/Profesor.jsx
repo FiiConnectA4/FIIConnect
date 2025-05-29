5a916e779df69314ca2be5ed0140e2a192a7ae0d
Bors Andrei-Flavius <flavisimi@gmail.com>
2025-05-29 17:11:46 +0300

    Catalog for professor bug solve

--- frontend/src/pages/Catalog/Profesor/Profesor.jsx
+++ frontend/src/pages/Catalog/Profesor/Profesor.jsx
@@ -35,6 +35,7 @@ const Profesor = () => {
         if (!profesorId) return;
         fetch(`/didactic/professor/${profesorId}`, { headers: { 'Authorization': `Bearer ${token}` } })
             .then(res => res.json())
+            .then(data => {
                 const courses = (data.courses || [])
                     .map(c => c.course)
                     .filter(c => c.archived !== 1);
